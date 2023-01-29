7aa8d323da9feb08fef4fa4174c45bc1bd7840cd
nianyiii <60533444+nianyiii@users.noreply.github.com>
2023-01-29 23:56:33 +0800

    Delete Level1-Development directory

--- Level1-Development/Day07_20210608_匿名函數與套件使用/modu.py
+++
@@ -1,9 +0,0 @@
-class test1:
-    def __init__(self):
-        print("create")
-def fun1( ):
-    print("function")
-    return( )
-#如果主程式不放在條件分析內呢?
-if __name__ == '__main__':
-    print("testmu!")
